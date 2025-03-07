844e056129dadb8e2b263b316c46f6ae13c8cf54
tuongviha <hathituongvi1452005@gmail.com>
2025-03-07 10:42:20 +0700

    Add files via upload

---
+++ BTTHs1.sql
@@ -0,0 +1,157 @@
+﻿
+use QLBH
+GO
+CREATE TABLE NhomSanPham (
+ [MaNhom] [int] PRIMARY KEY,
+ [TenNhom] [nvarchar](15) NULL )
+GO
+/****** Object: Table [dbo].[Nhanvien] ******/
+GO
+CREATE TABLE [dbo].[Nhanvien](
+ [MaNV] [nchar](5) PRIMARY KEY,
+ [TenNV] [nvarchar](40) NOT NULL,
+ [DiaChi] [nvarchar](60) NULL,
+ [Dienthoai] [nvarchar](24) NULL )
+GO
+/****** Object: Table [dbo].[NhaCungCap] ******/
+GO
+CREATE TABLE [dbo].[NhaCungCap](
+ [MaNCC] [int] NOT NULL,
+ [TenNcc] [nvarchar](40) NOT NULL,
+ [Diachi] [nvarchar](60) NULL,
+ [Phone] [nvarchar](24) NULL,
+ [SoFax] [nvarchar](24) NULL,
+ [DCMail] [nvarchar](50) NULL,
+PRIMARY KEY ( [MaNCC]) )
+GO
+/****** Object: Table [dbo].[KhachHang] ******/
+GO
+CREATE TABLE KhachHang(
+ MaKh nchar(5) NOT NULL,
+ TenKh nvarchar(40) NOT NULL,
+ LoaiKh nvarchar(3) NULL,
+ DiaChi nvarchar(60) NULL,
+ Phone nvarchar(24) NULL,
+PRIMARY KEY (MaKh) ) 
+GO
+
+ALTER TABLE KhachHang ADD SoFax NVarchar(24) 
+GO
+ALTER TABLE KhachHang ADD DCMail NVarchar(50)
+GO
+ALTER TABLE KhachHang ADD DiemTL Int
+GO
+/****** Object: Table [dbo].[SanPham] ******/
+CREATE TABLE [dbo].[SanPham](
+ [MaSp] [int] NOT NULL,
+ [TenSp] [nvarchar](40) NOT NULL,
+ [MaNCC] [int] NULL,
+ [MoTa] [nvarchar](50) NULL,
+ [MaNhom] [int] NULL,
+ [Đonvitinh] [nvarchar](20) NULL,
+ [GiaGoc] [money] NULL,
+ [SLTON] [int] NULL,
+PRIMARY KEY ([MaSp]) )
+GO
+/****** Object: Table [dbo].[HoaDon] ******/
+CREATE TABLE HoaDon(
+ MaHD int NOT NULL,
+ NgayLapHD datetime NULL,
+ NgayGiao datetime NULL,
+ Noichuyen nvarchar(60) NOT NULL,
+ MaNV nchar (5) NULL,
+ MaKh nchar (5) NULL,
+PRIMARY KEY ( MaHD) )
+GO
+/****** Object: Table [dbo].[CT_HoaDon] ******/
+GO
+CREATE TABLE [dbo].[CT_HoaDon](
+ [MaHD] [int] NOT NULL,
+ [MaSp] [int] NOT NULL,
+ [Soluong] [smallint] NULL,
+ [Dongia] [money] NULL,
+ [ChietKhau] [money] NULL,
+PRIMARY KEY CLUSTERED
+(
+ [MaHD] ASC,
+ [MaSp] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, 
+ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object: Default [HD_df] ******/
+ALTER TABLE [dbo].[HoaDon] ADD CONSTRAINT [HD_df] DEFAULT (getdate()) FOR
+[NgayLapHD]
+GO
+/****** Object: Check [kh_ck] ******/
+ALTER TABLE [dbo].[KhachHang] WITH CHECK ADD CONSTRAINT [kh_ck] CHECK 
+(([LoaiKH]='VL' OR [LoaiKH]='TV' OR [LoaiKH]='VIP'))
+GO
+ALTER TABLE [dbo].[KhachHang] CHECK CONSTRAINT [kh_ck]
+GO
+
+/****** Object: Check [Sanpham_ck] ******/
+ALTER TABLE [dbo].[SanPham] WITH CHECK ADD CONSTRAINT [Sanpham_ck] CHECK 
+(([giagoc]>(0)))
+GO
+ALTER TABLE [dbo].[SanPham] CHECK CONSTRAINT [Sanpham_ck]
+GO
+/****** Object: Check [Sanpham_ck1] ******/
+ALTER TABLE [dbo].[SanPham] WITH CHECK ADD CONSTRAINT [Sanpham_ck1] CHECK 
+(([slton]>(0)))
+GO
+ALTER TABLE [dbo].[SanPham] CHECK CONSTRAINT [Sanpham_ck1]
+GO
+/****** Object: Check [sp_ck] ******/
+ALTER TABLE [dbo].[SanPham] WITH CHECK ADD CONSTRAINT [sp_ck] CHECK (([SlTon]>(0)))
+GO
+ALTER TABLE [dbo].[SanPham] CHECK CONSTRAINT [sp_ck]
+GO
+/****** Object: Check [HD_ck] ******/
+ALTER TABLE [dbo].[HoaDon] WITH CHECK ADD CONSTRAINT [HD_ck] CHECK 
+(([NgayLapHD]>=getdate()))
+GO
+ALTER TABLE [dbo].[HoaDon] CHECK CONSTRAINT [HD_ck]
+GO
+/****** Object: Check [CK__CT_HoaDon__Chiet__1CF15040] ******/
+ALTER TABLE [dbo].[CT_HoaDon] WITH CHECK ADD CHECK (([chietkhau]>=(0)))
+GO
+/****** Object: Check [CK__CT_HoaDon__Soluo__1BFD2C07] ******/
+ALTER TABLE [dbo].[CT_HoaDon] WITH CHECK ADD CHECK (([soluong]>(0)))
+GO
+/****** Object: ForeignKey [FK__SanPham__MaNCC__08EA5793] ******/
+ALTER TABLE [dbo].[SanPham] WITH CHECK ADD FOREIGN KEY([MaNCC])
+REFERENCES [dbo].[NhaCungCap] ([MaNCC])
+GO
+/****** Object: ForeignKey [FK__SanPham__MaNhom__09DE7BCC] ******/
+ALTER TABLE [dbo].[SanPham] WITH CHECK ADD FOREIGN KEY([MaNhom])
+REFERENCES [dbo].[NhomSanPham] ([MaNhom])
+GO
+/****** Object: ForeignKey [FK__HoaDon__MaKh__173876EA] ******/
+ALTER TABLE [dbo].[HoaDon] WITH CHECK ADD FOREIGN KEY([MaKh])
+REFERENCES [dbo].[KhachHang] ([MaKh])
+GO
+/****** Object: ForeignKey [FK__HoaDon__MaNV__164452B1] ******/
+ALTER TABLE [dbo].[HoaDon] WITH CHECK ADD FOREIGN KEY([MaNV])
+REFERENCES [dbo].[Nhanvien] ([MaNV])
+GO
+/****** Object: ForeignKey [FK__CT_HoaDon__MaHD__1DE57479] ******/
+ALTER TABLE [dbo].[CT_HoaDon] WITH CHECK ADD FOREIGN KEY([MaHD])
+REFERENCES [dbo].[HoaDon] ([MaHD])
+GO
+/****** Object: ForeignKey [FK__CT_HoaDon__MaSp__1ED998B2] ******/
+ALTER TABLE [dbo].[CT_HoaDon] WITH CHECK ADD FOREIGN KEY([MaSp])
+REFERENCES [dbo].[SanPham] ([MaSp])
+GO
+----cau e
+ALTER TABLE HoaDon ADD LoaiHD Char(1) default 'N';
+GO
+ALTER TABLE HoaDon ADD CONSTRAINT ck_LoaiHD CHECK (LoaiHD 
+IN ('N', 'X', 'C', 'T'));
+
+----cau f
+ALTER TABLE HoaDon
+ADD CONSTRAINT ck_NgayGiao_NgayLapHD CHECK (NgayGiao >= NgayLapHD);
+GO
+
+--- câu g
---
+++ BTTHs2.sql
@@ -0,0 +1,172 @@
+﻿CREATE DATABASE Movies
+GO
+(
+    NAME = Movies_data,  
+    FILENAME = 'C:\\Users\\Haaviee\\Desktop\\CSDL\\tuan 4\\Movies_data.mdf',
+    SIZE = 25MB,
+    MAXSIZE = 40MB,
+    FILEGROWTH = 1MB
+)
+LOG ON 
+(
+    NAME = Movies_log,
+    FILENAME = 'C:\\Users\\Haaviee\\Desktop\\CSDL\\tuan 4\\BTTHs2\\Movies_log.ldf',
+    SIZE = 6MB,
+    MAXSIZE = 8MB,
+    FILEGROWTH = 1MB
+);
+
+ALTER DATABASE Movies 
+ADD FILE 
+(
+    NAME = Movies_data2,
+    FILENAME = 'C:\\Users\\Haaviee\\Desktop\\CSDL\\tuan 4\\BTTHs2\Movies_data2.ndf',
+    SIZE = 10MB
+);
+ALTER DATABASE Movies SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+
+ALTER DATABASE Movies SET RESTRICTED_USER;
+
+ALTER DATABASE Movies SET MULTI_USER;
+
+ALTER DATABASE Movies
+MODIFY FILE 
+(
+    NAME = Movies_data2,
+    SIZE = 15 MB,
+);
+
+ALTER DATABASE Movies 
+SET AUTO_SHRINK ON;
+
+DROP DATABASE Movies;
+
+
+ALTER DATABASE Movies
+MODIFY FILE
+(
+    NAME = Movies_log,
+    MAXSIZE = 10MB
+);
+
+ALTER DATABASE Movies 
+MODIFY FILE 
+(
+    NAME = Movies_data2,
+    SIZE = 10MB
+);
+
+ALTER DATABASE Movies 
+MODIFY FILE 
+(
+    NAME = Movies_data2,
+    SIZE = 10MB
+);
+
+ALTER DATABASE Movies 
+MODIFY FILE 
+(
+    NAME = Movies_data2,
+    FILEGROUP = Data
+);
+
+
+
+
+USE Movies;
+
+CREATE TABLE Movie (
+    MovieID INT PRIMARY KEY IDENTITY,
+    Title NVARCHAR(255) NOT NULL,
+    ReleaseYear INT,
+    Genre NVARCHAR(100)
+);
+
+CREATE TABLE Customer (
+    CustomerID INT PRIMARY KEY IDENTITY,
+    FullName NVARCHAR(255) NOT NULL,
+    Email NVARCHAR(255) UNIQUE,
+    Phone NVARCHAR(20)
+);
+
+CREATE TABLE Category (
+    CategoryID INT PRIMARY KEY IDENTITY,
+    CategoryName NVARCHAR(255) NOT NULL
+);
+
+USE Movies;
+
+-- Tạo CSDL
+CREATE DATABASE Movies;
+GO
+USE Movies;
+GO
+
+-- Định nghĩa User-Defined Data Types
+CREATE TYPE Movie_num FROM INT NOT NULL;
+CREATE TYPE Category_num FROM INT NOT NULL;
+CREATE TYPE Cust_num FROM INT NOT NULL;
+CREATE TYPE Invoice_num FROM INT NOT NULL;
+GO
+
+-- Tạo bảng Customer
+CREATE TABLE Customer (
+    Cust_num Cust_num IDENTITY(300,1) PRIMARY KEY,
+    Lname VARCHAR(20) NOT NULL,
+    Fname VARCHAR(20) NOT NULL,
+    Address1 VARCHAR(30) NULL,
+    Address2 VARCHAR(20) NULL,
+    City VARCHAR(20) NULL,
+    State CHAR(2) NULL,
+    Zip CHAR(10) NULL,
+    Phone VARCHAR(10) NOT NULL,
+    Join_date SMALLDATETIME NOT NULL DEFAULT GETDATE()
+);
+GO
+
+-- Tạo bảng Category
+CREATE TABLE Category (
+    Category_num Category_num IDENTITY(1,1) PRIMARY KEY,
+    Description VARCHAR(20) NOT NULL
+);
+GO
+
+-- Tạo bảng Movie
+CREATE TABLE Movie (
+    Movie_num Movie_num PRIMARY KEY,
+    Title VARCHAR(100) NOT NULL,
+    Category_Num Category_num NOT NULL,
+    Date_purch SMALLDATETIME NULL DEFAULT GETDATE(),
+    Rental_price INT NULL,
+    Rating CHAR(5) NULL,
+    CONSTRAINT FK_movie FOREIGN KEY (Category_Num) REFERENCES Category(Category_num)
+);
+GO
+
+-- Tạo bảng Rental
+CREATE TABLE Rental (
+    Invoice_num Invoice_num PRIMARY KEY,
+    Cust_num Cust_num NOT NULL,
+    Rental_date SMALLDATETIME NOT NULL DEFAULT GETDATE(),
+    Due_date SMALLDATETIME NOT NULL DEFAULT DATEADD(DAY, 2, GETDATE()),
+    CONSTRAINT FK_rental FOREIGN KEY (Cust_num) REFERENCES Customer(Cust_num)
+);
+GO
+
+-- Tạo bảng Rental_Detail
+CREATE TABLE Rental_Detail (
+    Invoice_num Invoice_num NOT NULL,
+    Line_num INT NOT NULL,
+    Movie_num Movie_num NOT NULL,
+    Rental_price SMALLMONEY NULL,
+    PRIMARY KEY (Invoice_num, Line_num),
+    CONSTRAINT FK_detail_invoice FOREIGN KEY (Invoice_num) REFERENCES Rental(Invoice_num) ON DELETE CASCADE,
+    CONSTRAINT FK_detail_movie FOREIGN KEY (Movie_num) REFERENCES Movie(Movie_num)
+);
+GO
+
+-- Ràng buộc CHECK
+ALTER TABLE Movie ADD CONSTRAINT CK_movie CHECK (Rating IN ('G', 'PG', 'R', 'NC17', 'NR'));
+ALTER TABLE Rental ADD CONSTRAINT CK_Due_date CHECK (Due_date >= Rental_date);
+GO
+
